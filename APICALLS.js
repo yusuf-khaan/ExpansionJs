a04d7cb560ba353603f5a3c5abe94bab3d2b6260
yusuf-khaan <142320884+yusuf-khaan@users.noreply.github.com>
2024-12-24 12:32:17 +0530

    APICALLS.js

--- APICALLS.js
+++ APICALLS.js
@@ -22,8 +22,29 @@ function fetchWeatherData(city) {
 const city = 'London';
 fetchWeatherData(city)
     .then(response => {
-        console.log('API call successful:', response);
+        console.log("API call successful:", response);
     })
     .catch(error => {
-        console.error('API call failed:', error);
+        console.error("API call failed:", error);
     });
+
+
+
+//using jsonplaceholder for title and 
+
+const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+
+fetch(apiUrl, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log("Posted data titles:", data.title, "Posted data userID ", data.userId));
+  .catch((error) => console.error('Error making the POST request:', error));
